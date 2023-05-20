ca65c867090feec4cc0af49d6a900c55845ce4bb
Jean (Jeto) <jeangasparello@hotmail.com>
2023-05-20 13:44:53 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   botjitacode/jitabot.py

--- botjitacode/jitabot.py
+++ botjitacode/jitabot.py
@@ -6,7 +6,7 @@ from func_repetiveis import *
 # Configuraçãoes e Importação JSON dos Canais.
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='%', intents=intents)
-token = 'MTEwNzgxODA5ODkwMTQwMTYwMA.GN_7iP.QDogER0eQTizhX7nNWrafzfkdykT03fhkwJnb4'
+token = 'MTEwNzgxODA5ODkwMTQwMTYwMA.GPLWrG.VdPBfjPXt2KcOX_RQvaOh7zgrrkZuoT8ZKkFYU'
 
 
 # Dicionário para armazenar os canais de contagem (ID do servidor e ID do canal)
