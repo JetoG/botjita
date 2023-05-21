9d4808f7de8d9fffb2f84639c2067a9d41855403
Jean (Jeto) <jeangasparello@hotmail.com>
2023-05-21 17:57:06 -0300

    Feito pequenas otimizações no código.  Adicionado Função de Troca.
     Na Branch Main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   botjitacode/func_repetiveis.py
    	modified:   botjitacode/func_utils.py
    	modified:   botjitacode/main.py

--- botjitacode/func_repetiveis.py
+++ botjitacode/func_repetiveis.py
@@ -2,9 +2,89 @@ import discord
 import asyncio
 from func_utils import *
 
+# ENVIAR NOTIFICAÇÕES
+async def enviar_notificacoes(recipients, title, description):
+    embed = discord.Embed(title=title, description=description)
+    for recipient in recipients:
+        await recipient.send(embed=embed)
+
+# CANAL MEMBROS
+# Função para verificar o canal de membros no evento on_ready
+notificacao_enviada_membros = False
+async def verifica_canal_membros_on_ready(guild):
+    global notificacao_enviada_membros
+
+    channel_id = await get_member_count_channel(guild)
+    if not channel_id:
+        if not notificacao_enviada_membros:
+            owner = guild.owner
+            admins = [
+                member for member in guild.members if member.guild_permissions.administrator]
+            recipients = [owner] + admins
+            title = "Canal de Contagem de Membros"
+            description = '''
+                O Canal de Contagem de Membros, não existe ou foi deletado.
+                Caso deseje criar o canal novamente utilize o comando:
+                **%criarcanalmembros**
+                *Observação: Essa Mensagem será apagada após 24 horas...*
+            '''
+            msgnoti = await enviar_notificacoes(recipients, title, description)
+            notificacao_enviada_membros = True
+
+            await asyncio.sleep(24 * 60 * 60)
+            if msgnoti:
+                await msgnoti.delete()
+        return None
+     
+    channel = guild.get_channel(int(channel_id))
+    return channel
+
+
+# Função de Verificação e de Criar o canal ao entrar no Servidor
 async def verifica_canal_membros(guild):
-        channel_id = await get_member_count_channel(guild)
-        if not channel_id:
-            channel = await create_member_count_channel(guild)
-        else:
-            channel = guild.get_channel(int(channel_id))
+    channel_id = await get_member_count_channel(guild)
+    if not channel_id:
+        channel = await create_member_count_channel(guild)
+    else:
+        channel = guild.get_channel(int(channel_id))
+
+
+
+# CANAL TROCAS
+# Função para verificar o canal de trocas no evento on_ready
+notificacao_enviada_trocas = False
+async def verifica_canal_trade_on_ready(guild, bot):
+    global notificacao_enviada_trocas
+
+    channel_id = await get_trade_notifications_channel(guild)
+    if not channel_id:
+        if not notificacao_enviada_trocas:
+            owner = guild.owner
+            admins = [
+                member for member in guild.members if member.guild_permissions.administrator]
+            recipients = [owner] + admins
+            title = "Canal de Trocas do Servidor"
+            description = '''
+                O Canal de Trocas do Servidor, não existe ou foi deletado.
+                Caso deseje criar o canal novamente utilize o comando:
+                **%criarcanaltrocas**
+                *Observação: Essa Mensagem será apagada após 24 horas...*
+            '''
+            msgnoti = await enviar_notificacoes(recipients, title, description)
+            notificacao_enviada_trocas = True
+
+            await asyncio.sleep(24 * 60 * 60)
+            if msgnoti:
+                await msgnoti.delete()
+        return None
+
+    channel = guild.get_channel(int(channel_id))
+    return channel
+
+# Função de Verificação e de Criar o canal ao entrar no Servidor
+async def verifica_canal_trade(guild):
+    channel_id = await get_trade_notifications_channel(guild)
+    if not channel_id:
+        channel = await create_trade_notifications_channel(guild)
+    else:
+        channel = guild.get_channel(int(channel_id))
